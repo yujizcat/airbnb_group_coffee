7e540108952780710b29825984abc5b99fb5b77d
yuji zhou <yujizcat@outlook.com>
2022-04-25 15:47:13 +0800

    add

---
+++ config/initializers/devise.rb
@@ -0,0 +1,7 @@
+# config/initializers/devise.rb
+
+# ==> Mailer Configuration
+# Configure the e-mail address which will be shown in Devise::Mailer,
+# note that it will be overwritten if you use your own mailer class
+# with default "from" parameter.
+config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
